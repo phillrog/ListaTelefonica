4daeec37e9e8a4697ff6c2ab060c358554dbf39b
Phillipe Souza <phillipe.souza@fh.com.br>
2019-11-10 15:24:42 -0300

    configurando banco

--- ListaTelefonica.API/Startup.cs
+++ ListaTelefonica.API/Startup.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ListaTelefonica.Infra.Data.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +11,22 @@ namespace ListaTelefonica.API
 {
 	public class Startup
 	{
+		IConfiguration Configuration { get; set; }
+		public Startup(IConfiguration configuration)
+		{
+			var builder = new ConfigurationBuilder().AddJsonFile("config.json");
+			Configuration = builder.Build();
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			var conn = Configuration.GetConnectionString("ListaTelefonica");
+
+			services.AddDbContext<ListaTelefoneContext>(option => 
+					option.UseSqlServer(conn, m => m.MigrationsAssembly("ListaTelefonica.Infra.Data")));
+			services.AddMvcCore();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
